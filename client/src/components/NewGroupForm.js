d23ea1c556bfdb68a14f9a06633b53afe073da77
Alex <pich.alex01@gmail.com>
2020-09-10 17:40:47 -0700

    Handle a file upload to accept an image and uploads it to Cloudinary as well as POSTing to the database when submitting the form

--- client/src/components/NewGroupForm.js
+++ client/src/components/NewGroupForm.js
@@ -1,17 +1,31 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Button, Form } from "react-bootstrap";
+import styled from "styled-components";
+import axios from "axios";
 import CategoryService from "../services/category.service";
 import GroupService from "../services/group.service";
 
-const NewGroupForm = () => {
-  const [categories, setCategories] = useState([]);
+const ImageInput = styled.div`
+  background: #ededed;
+  width: 160px;
+  height: 100px;
+`;
 
+const Image = styled.img`
+  width: 160px;
+  height: 100px;
+`;
+
+const NewGroupForm = () => {
   const initialFormState = {
     categoryId: 17,
     groupName: "Placeholder",
     userId: 1,
   };
 
+  const [loading, setLoading] = useState(false);
+  const [loadingProgress, setLoadingProgress] = useState("");
+  const [categories, setCategories] = useState([]);
   const [categoryId, setCategoryId] = useState(initialFormState.categoryId); // Set default value to Art
   const [groupName, setGroupName] = useState(initialFormState.groupName);
   // const [location, setLocation] = useState("");
@@ -41,16 +55,56 @@ const NewGroupForm = () => {
   //   });
   // };
 
+  let initialImageInput = null;
+  // Upload image
+  const uploadImage = async (e) => {
+    setLoading(true);
+
+    let id = e.target.id;
+    console.log("the uploaded id is: " + id);
+
+    console.log("Uploading image...");
+    const files = e.target.files;
+    const data = new FormData();
+    data.append("file", files[0]);
+    data.append("upload_preset", "wut-group");
+
+    // Upload to Cloudinary
+    const res = await axios.post(
+      "https://api.cloudinary.com/v1_1/bpeach/image/upload",
+      data,
+      {
+        onUploadProgress: (progressEvent) => {
+          setLoadingProgress(
+            "Upload progress: " +
+              Math.round((progressEvent.loaded / progressEvent.total) * 100) +
+              "%"
+          );
+        },
+      }
+    );
+    const imageData = res.data.secure_url;
+    setImageUrl(imageData);
+
+    setLoading(false);
+  };
+
   const submitForm = (e) => {
     e.preventDefault();
 
     let data = {
       group_name: groupName,
+      image_url: imageUrl,
       category_id: categoryId,
       user_id: userId,
     };
 
-    GroupService.createOne(data.group_name, data.category_id, data.user_id);
+    GroupService.createOne(
+      data.group_name,
+      data.image_url,
+      data.category_id,
+      data.user_id
+    );
     console.log("Group created successfully!");
   };
 
@@ -82,6 +136,33 @@ const NewGroupForm = () => {
           onChange={handleGroupNameChange}
         />
       </Form.Group>
+      <Form.Group>
+        <Form.Label>Upload Group Photo</Form.Label>
+        {imageUrl ? (
+          <ImageInput>
+            <div>
+              <Image src={imageUrl} alt="Group Image" />
+            </div>
+          </ImageInput>
+        ) : (
+          <ImageInput
+            onClick={() => {
+              initialImageInput.click();
+            }}
+          ></ImageInput>
+        )}
+        {loading ? <p>{loadingProgress}</p> : ""}
+      </Form.Group>
+      <input
+        style={{ display: "none" }}
+        type="file"
+        name="file"
+        placeholder="Upload an image"
+        required
+        onChange={uploadImage}
+        ref={(fileInput) => (initialImageInput = fileInput)}
+      />
+
       {/* TODO: LOCATION Input */}
       {/* <Form.Group controlId="formGroupLocation">
         <Form.Label>Location</Form.Label>
