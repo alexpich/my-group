d7251e0ac132fc1513004034c171dc95a13d24e5
Alex <pich.alex01@gmail.com>
2020-09-23 01:49:33 -0700

    Conditionally render nav

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,8 +1,35 @@
 import React from "react";
 import { Navbar, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 
-const Navigation = () => {
+const Navigation = (props) => {
+  const renderLinks = () => {
+    if (props.authenticated) {
+      return (
+        <>
+          <Nav.Link to="/signout" as={Link}>
+            <Navbar.Text>Sign Out</Navbar.Text>
+          </Nav.Link>
+          <Nav.Link to="/feature" as={Link}>
+            <Navbar.Text>Feature</Navbar.Text>
+          </Nav.Link>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <Nav.Link to="/signup" as={Link}>
+            <Navbar.Text>Sign Up</Navbar.Text>
+          </Nav.Link>
+          <Nav.Link to="/signin" as={Link}>
+            <Navbar.Text>Sign In</Navbar.Text>
+          </Nav.Link>
+        </>
+      );
+    }
+  };
+
   return (
     <Navbar bg="light">
       <Navbar.Collapse>
@@ -13,23 +40,15 @@ const Navigation = () => {
           <Nav.Link to="/categories" as={Link}>
             <Navbar.Text>Discover</Navbar.Text>
           </Nav.Link>
-          <Nav.Link to="/feature" as={Link}>
-            <Navbar.Text>Feature</Navbar.Text>
-          </Nav.Link>
-          {/* TODO: conditionally show signin/signout based on login */}
-          <Nav.Link to="/signout" as={Link}>
-            <Navbar.Text>Sign Out</Navbar.Text>
-          </Nav.Link>
-          <Nav.Link to="/signup" as={Link}>
-            <Navbar.Text>Sign Up</Navbar.Text>
-          </Nav.Link>
-          <Nav.Link to="/signin" as={Link}>
-            <Navbar.Text>Sign In</Navbar.Text>
-          </Nav.Link>
+          {renderLinks()}
         </Nav>
       </Navbar.Collapse>
     </Navbar>
   );
 };
 
-export default Navigation;
+function mapStateToProps(state) {
+  return { authenticated: state.auth.authenticated };
+}
+
+export default connect(mapStateToProps)(Navigation);
